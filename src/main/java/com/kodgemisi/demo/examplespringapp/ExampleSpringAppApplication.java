066380069224f14c0cba635a615089113fbff20a
Zafer CAKMAK <mzcakmak@gmail.com>
2018-10-07 20:12:01 +0300

    initial commit

---
+++ src/main/java/com/kodgemisi/demo/examplespringapp/ExampleSpringAppApplication.java
@@ -0,0 +1,12 @@
+package com.kodgemisi.demo.examplespringapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleSpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleSpringAppApplication.class, args);
+	}
+}
---
+++ src/main/java/com/kodgemisi/demo/examplespringapp/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.kodgemisi.demo.examplespringapp.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created on October, 2018
+ *
+ * @author xaph
+ */
+@RestController
+public class HelloController {
+	@RequestMapping("/")
+	public String index() {
+		return "Greetings from Spring Boot!";
+	}
+}
