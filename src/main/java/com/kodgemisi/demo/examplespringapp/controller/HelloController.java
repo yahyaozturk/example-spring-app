96f8865fe3b83222b3afe1aa1742a12b76210f56
Yahya Öztürk <yahyaozturk@gmail.com>
2018-11-06 12:31:42 +0300

    Update HelloController.java

--- src/main/java/com/kodgemisi/demo/examplespringapp/controller/HelloController.java
+++ src/main/java/com/kodgemisi/demo/examplespringapp/controller/HelloController.java
@@ -12,6 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot!";
+		return "Greetings from Yahya Ozturk!";
 	}
 }
